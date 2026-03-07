f368a56f5dd110e9d6ed42f10e1afcee255ce6a9
Khushbakht <kbirfan2007@gmail.com>
2026-03-07 07:07:25 +0500

    feat: implement RPSLS using data-driven O(1) win-loss lookup and stateful scoring

---
+++ games/rock_paper_scissors_lizard_spock.py
@@ -0,0 +1,55 @@
+import random
+
+def try_user_choice():
+    try:
+        user_choice = input("User Choice(q to exit): ").lower()
+        if not user_choice.isalpha() or user_choice not in {"rock", "paper", "scissors", "lizard", "spock", "q"}:
+            raise ValueError
+        else:
+            return user_choice
+    except ValueError:
+        print("Value Error:\nEnter a correct value as per the choices")
+    except Exception as e:
+        print(f"Error:\n{e}")
+
+def checking(user_validated_choice, computer_choice, points):
+    rules = {
+        "rock": ["scissors", "lizard"],
+        "paper": ["rock", "spock"],
+        "scissors": ["paper", "lizard"],
+        "lizard": ["spock", "paper"],
+        "spock": ["scissors", "rock"]
+    }
+    if computer_choice in rules[user_validated_choice]:
+        print("You win! (+1) ")
+        points += 1
+    elif user_validated_choice == computer_choice:
+        print("Tie!")
+    else:
+        print("You lose! (-1)")
+        points -= 1
+
+    return points
+
+
+print("Rock | Paper | Scissors")
+points = 10
+
+while True:
+    print("====================================================")
+    choices = ["rock", "paper", "scissors", "lizard", "spock"]
+    computer_choice = random.choice(choices)
+
+    user_validated_choice = try_user_choice()
+    if user_validated_choice == "q":
+        print(f"Points: {points}")
+        print("Goodbye!")
+        break
+    if user_validated_choice:
+        print(f"Computer Choice: {computer_choice}")
+        points = checking(user_validated_choice, computer_choice, points)
+        print(f"Points: {points}")
+        if points <= 0:
+            print("Game Over!")
+            break
+        print("====================================================")
