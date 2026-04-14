a0e8e1f0a0ca93e170892e315b417772f9d7f564
Khushbakht <kbirfan2007@gmail.com>
2026-04-14 17:15:57 +0500

    feat: add CSV persistence and file handling for inventory data

--- apps/inventory_manager.py
+++ apps/inventory_manager.py
@@ -1,8 +1,25 @@
-inventory = {"apples": 10,
-            "bananas" : 2,
-            "mangoes" : 15,
-            "peaches" : 1,
-            "pomegranates" : 9}
+import csv
+
+file_path = "inventory.csv"
+
+def load_from_csv():
+    inventory = []
+    try:
+        with open(file_path, "r") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                item = row[0]
+                qty = int(row[1])
+                inventory.append([item, qty])
+    except FileNotFoundError:
+        pass
+    return inventory
+
+def save_to_csv(inventory):
+    with open(file_path, "w", newline="") as file:
+        writer = csv.writer(file)
+        for row in inventory:
+            writer.writerow(row)
 
 def select_option():
     print("== INVENTORY MANAGER ==")
@@ -26,46 +43,80 @@ def validate_user_option():
 
 def see_inventory():
     print("============================")
-    print("== INVENTORY ==")
-    for key, value in inventory.items():
-        print(f"{key} : {value}")
+    print("====== INVENTORY ======")
+    try:
+        with open(file_path, "r") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                print(f"{row[0].capitalize():<15} : {row[1]}")
+    except FileNotFoundError:
+        print("File not found")
     print("============================")
 
 def update_stock(inventory):
     user_item = input("Item: ").strip().lower()
-    if user_item in inventory:
-        user_quantity = int(input("Qty: "))
-        if user_quantity < 0:
-            print("Qty can't be negative")
-        else:
-            inventory.update({user_item : user_quantity})
-            print("Updated!")
+    for item in inventory:
+        if item[0] == user_item:
+            try:
+                user_quantity = int(input("Qty: "))
+
+                if user_quantity < 0:
+                    print("Qty can't be negative")
+                else:
+                    item[1] = user_quantity
+                    save_to_csv(inventory)
+                    print("Updated!")
+                return
+            except ValueError:
+                print("Invalid input")
+                return
     else:
         print("Item not in inventory")
 
-def new_item():
+def new_item(inventory):
     user_new_item = input("New Item: ").strip().lower()
-    if not user_new_item in inventory:
+    for item in inventory:
+        if item[0] == user_new_item:
+            print("Item already in inventory")
+            return
+    try:
         user_new_qty = int(input("Qty: "))
         if user_new_qty < 0:
             print("Qty can't be negative")
         else:
-            inventory[user_new_item] = user_new_qty
+            inventory.append([user_new_item, user_new_qty])
+            save_to_csv(inventory)
             print("Added!")
-    else:
-        print("Item already in inventory")
+    except ValueError:
+        print("Invalid input")
 
-def check_3_units():
+def check_3_units(inventory):
     print("=============================")
     print("Need to be restocked")
-    for key, value in inventory.items():
-        if value < 3:
-            print(f"{key} : {value}")
+    for item in inventory:
+        if item[1] < 3:
+            print(f"{item[0]} : {item[1]}")
     print("=============================")
 
+
+inventory = load_from_csv()
+
+if not inventory:
+    inventory = [["apples", 10],
+                 ["bananas", 2],
+                 ["mangoes", 15],
+                 ["peaches", 1],
+                 ["pomegranates", 9]]
+    save_to_csv(inventory)
+
+
 select_option()
+
 while True:
     selected_option = validate_user_option()
+
+    if selected_option is None:
+        continue
 
     if selected_option == "1":
         see_inventory()
@@ -74,10 +125,10 @@ while True:
         update_stock(inventory)
 
     elif selected_option == "3":
-        new_item()
+        new_item(inventory)
 
     elif selected_option == "4":
-        check_3_units()
+        check_3_units(inventory)
 
     elif selected_option == "5":
         break
